ea7d39641206e29f80a8a6e46f37807adb8b65ca
Ivan Valdes <iv@nvald.es>
2015-03-22 03:08:40 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM haproxy:1.5
+MAINTAINER Ivan Valdes <ivan@vald.es>
+
+RUN apt-get update && apt-get install -y --no-install-recommends procps curl && \
+    curl -kL https://github.com/kelseyhightower/confd/releases/download/v0.8.0/confd-0.8.0-linux-amd64 > /usr/local/bin/confd && \
+    chmod +x /usr/local/bin/confd &&\
+    mkdir -p /etc/confd/conf.d && \
+    mkdir -p /etc/confd/templates 
+
+RUN mkdir /haproxy
+
+ADD files/boot.sh /haproxy/boot.sh
+ADD files/haproxy.cfg.tmpl /etc/confd/templates/haproxy.cfg.tmpl
+ADD files/haproxy.toml /etc/confd/conf.d/haproxy.toml
+
+CMD ["bash" "/haproxy/boot.sh"]
---
+++ files/boot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Fail hard and fast
+set -eo pipefail
+
+export ETCD_PORT=${ETCD_PORT:-4001}
+export HOST_IP=${HOST_IP:-172.17.42.1}
+export ETCD=$HOST_IP:4001
+
+echo "[haproxy] booting container. ETCD: $ETCD"
+
+# Loop until confd has updated the haproxy config
+until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/haproxy.toml; do
+  echo "[haproxy] waiting for confd to refresh haproxy.cfg"
+  sleep 5
+done
+
+# Run confd in the background to watch the upstream servers
+confd -interval 10 -node $ETCD -config-file /etc/confd/conf.d/haproxy.toml &
+echo "[haproxy] confd is listening for changes on etcd..."
+
+# Start haproxy
+echo "[haproxy] starting haproxy service..."
+haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+
+# Tail all nginx log files
+tail -f /var/log/syslog
